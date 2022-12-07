4ccd34347040c16bb4ff1195500a55f041d8d8fb
chicheva <chicheva-e@mail.ru>
2022-12-07 23:54:23 +0300

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход принимает два числа и выдает 
+// какое число больше и какое число меньше
+
+Console.WriteLine("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (a>b){
+    Console.WriteLine($"Число а = {a} максимальное, а число b = {b} минимальное");
+}
+else {
+    Console.WriteLine($"Число а = {a} минимальное, а число b = {b} максимальное");
+}
