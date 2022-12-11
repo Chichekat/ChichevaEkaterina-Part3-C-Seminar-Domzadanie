55489a7d7ee4a43518f515fe4634175a31e74376
chicheva <chicheva-e@mail.ru>
2022-12-11 12:39:58 +0300

    Задача 6

---
+++ Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+if (N % 2 == 0){
+    Console.WriteLine($"Число {N} четное");
+}
+else {
+    Console.WriteLine($"Число {N} не четное");
+}
